5287f9e7338c1ae86cc04ac9a0647cb8ff35d9f9
Shashank Veerapaneni <vshashank@juniper.net>
2020-12-02 20:48:46 +0530

    debug

--- src/helm_image_mirror.py
+++ src/helm_image_mirror.py
@@ -140,9 +140,10 @@ class Repo:
         return add_cmd
 
     def add(self):
-        cmd = self.get_add_cmd()
+        cmd = self.get_add_cmd(mask_pw=False)
         masked_cmd = self.get_add_cmd(mask_pw=True)
         print(masked_cmd)
+        print(cmd)
         helm(cmd, print_cmd=False)
 
     def __eq__(self, other):
