a674d8bfc9efb5f8ba2fafc6893890a1f81fc9b5
Shashank Veerapaneni <shashank.veerapaneni@reddit.com>
2022-12-01 18:24:17 -0600

    Update help text

--- helm_image_mirror.sh
+++ helm_image_mirror.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-IMAGE=shashankv/helm_image_mirror:v2.0.1
+IMAGE=shashankv/helm_image_mirror:v2.1.0
 docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:/workdir $IMAGE "$@"
